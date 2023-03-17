26b657d1a9328572067364b5f31cbe89cac621cc
qin <qinqiaomeng@outlook.com>
2023-03-17 17:22:48 -0400

    teleop C++

--- src/qcar/control.sh
+++ src/qcar/control.sh
@@ -6,4 +6,4 @@ source /opt/ros/melodic/setup.bash
 source ~/qcar_ws/devel/setup.bash
 export ROS_IP=192.168.1.107
 export ROS_MASTER_URI=http://192.168.1.120:11311
-roslaunch qcar control.launch
+roslaunch qcar slam.launch

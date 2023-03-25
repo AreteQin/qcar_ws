59706a6d7e534cdca29edddbc6f26da52cd25c10
qin <qinqiaomeng@outlook.com>
2023-03-25 15:57:27 -0400

    test

--- src/qcar/control_59.sh
+++ src/qcar/control_59.sh
@@ -6,4 +6,4 @@ source /opt/ros/melodic/setup.bash
 source ~/qcar_ws/devel/setup.bash
 export ROS_IP=100.69.33.37
 export ROS_MASTER_URI=http://100.86.255.112:11311
-roslaunch qcar slam.launch
+roslaunch qcar control.launch

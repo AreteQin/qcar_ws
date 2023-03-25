0cafc2713b3b43993dd0bd52fba4a35bb0cea63a
qin <qinqiaomeng@outlook.com>
2023-03-25 15:15:59 -0400

    imu publisher

--- src/qcar/imu_publisher.sh
+++ src/qcar/imu_publisher.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-ls -l /dev |grep ttyTHS2
-sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
 source ~/qcar_ws/devel/setup.bash
 export ROS_IP=100.85.255.99
 export ROS_MASTER_URI=http://100.86.255.112:11311
-roslaunch qcar slam.launch
+chmod +x /home/nvidia/qcar_ws/src/qcar/src/IMU_publisher.py
+rosrun qcar IMU_publisher.py

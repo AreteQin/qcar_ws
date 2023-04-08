b08de22bc503d8168feefd0982ebf1ceb8542c7c
qin <qinqiaomeng@outlook.com>
2023-04-08 19:40:36 -0400

    rgbd done

--- src/qcar/src/D435_publisher.cpp
+++ src/qcar/src/D435_publisher.cpp
@@ -16,14 +16,16 @@ int main(int argc, char **argv) {
     rs2::pipeline pipe;
     rs2::config cfg;
     cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
+    cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
     // Start streaming with the default recommended configuration
     pipe.start(cfg);
 
     //image_transport will publish the video that can be compressed
     image_transport::ImageTransport it(nh);
     image_transport::Publisher pub_color = it.advertise("/camera/color/image_raw", 1);
+    image_transport::Publisher pub_depth = it.advertise("/camera/depth/image_rect_raw", 1);
 
-    cv::Mat color_cv;
+    cv::Mat color_cv, depth_cv;
 
     while (ros::ok()) {
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera (must be called in a loop)
@@ -33,14 +35,19 @@ int main(int argc, char **argv) {
         timesptamp.stamp = ros::Time::now();
 //    rs2::frame color_frame = color_map.colorize(data.get_color_frame());
         rs2::frame color_frame = data.get_color_frame();
+        rs2::frame depth_frame = data.get_depth_frame();
+
         // Query frame size (width and height)
 //        const int w = depth_frame.as<rs2::video_frame>().get_width();
 //        const int h = depth_frame.as<rs2::video_frame>().get_height();
 
         // Create OpenCV matrix of size (w,h) from the colorized depth data
         color_cv = cv::Mat(cv::Size(640, 480), CV_8UC3, (void *) color_frame.get_data(), cv::Mat::AUTO_STEP);
+        depth_cv = cv::Mat(cv::Size(640, 480), CV_16UC1, (void *) depth_frame.get_data(), cv::Mat::AUTO_STEP);
 
         pub_color.publish(cv_bridge::CvImage(timesptamp, "bgr8", color_cv).toImageMsg());
+        pub_depth.publish(cv_bridge::CvImage(timesptamp, "mono16", depth_cv).toImageMsg());
+
         ros::spinOnce();
     }
 }

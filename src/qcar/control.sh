f6792f4d1682bb31de9d2603c62f4d27382a8a1a
qin <qinqiaomeng@outlook.com>
2023-07-03 18:29:05 -0400

    rosip

--- src/qcar/control.sh
+++ src/qcar/control.sh
@@ -4,8 +4,8 @@ ls -l /dev |grep ttyTHS2
 sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
 source ~/qcar_ws/devel/setup.bash
-#export ROS_IP=192.168.1.107
-#export ROS_MASTER_URI=http://192.168.1.120:11311
-export ROS_IP=100.85.255.99
-export ROS_MASTER_URI=http://100.86.255.112:11311
-roslaunch qcar slam.launch
+export ROS_IP=192.168.1.74
+export ROS_MASTER_URI=http://192.168.1.12:11311
+#export ROS_IP=100.85.255.99
+#export ROS_MASTER_URI=http://100.86.255.112:11311
+roslaunch qcar control.launch

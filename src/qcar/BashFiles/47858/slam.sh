47fca5c9087e2b0a1353b975238254ddee82592a
Qin <you@example.com>
2023-07-25 18:41:10 -0400

    IMU to imu

--- src/qcar/BashFiles/47858/slam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ls -l /dev |grep ttyTHS2
-sudo chmod 666 /dev/ttyTHS2
-source /opt/ros/melodic/setup.bash
-source ~/qcar_ws/devel/setup.bash
-export ROS_IP=192.168.1.95
-export ROS_MASTER_URI=http://192.168.1.12:11311
-roslaunch qcar slam.launch
--- src/qcar/BashFiles/47859/imu_publish.sh
+++ src/qcar/BashFiles/47859/imu_publish.sh
@@ -6,4 +6,4 @@ source /opt/ros/melodic/setup.bash
 source ~/qcar_ws/devel/setup.bash
 export ROS_IP=192.168.1.74
 export ROS_MASTER_URI=http://192.168.1.12:11311
-rosrun qcar IMU_publisher.py
+rosrun qcar imu_publisher.py

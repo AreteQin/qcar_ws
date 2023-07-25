1c65b36af21a78de4528fc5d43beda205fa346c0
Qin <you@example.com>
2023-07-25 18:38:16 -0400

    d435 and imu launch file

--- src/qcar/BashFiles/47859/lidar_slam.sh
+++ src/qcar/BashFiles/47859/lidar_slam.sh
@@ -6,4 +6,4 @@ source /opt/ros/melodic/setup.bash
 source ~/qcar_ws/devel/setup.bash
 export ROS_IP=192.168.1.74
 export ROS_MASTER_URI=http://192.168.1.12:11311
-roslaunch qcar slam.launch
+roslaunch qcar lidar_slam.launch

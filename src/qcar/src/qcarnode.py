555f51a27320b2b66a474c8bd1811ec154cd3771
Qin <you@example.com>
2023-08-02 15:53:55 -0400

    qcarnode.py publish imu

--- src/qcar/src/qcarnode.py
+++ src/qcar/src/qcarnode.py
@@ -13,57 +13,73 @@ from geometry_msgs.msg import Vector3Stamped
 from sensor_msgs.msg import BatteryState
 import time
 
+# ros
+from sensor_msgs.msg import Imu
+
 class QcarNode(object):
 
-	def __init__(self):
-		super().__init__()
-		self.battery_pub_ = rospy.Publisher('/qcar/battery_state', BatteryState, queue_size=10)
-		self.carvel_pub_ = rospy.Publisher('/qcar/velocity', Vector3Stamped, queue_size=10)
-
-		self.myCar = QCar()
-		self.sample_time = 0.001
-		self.command = np.array([0, 0])
-		self.cmd_sub_ = rospy.Subscriber('/qcar/user_command', Vector3Stamped, self.process_cmd, queue_size=100)
+    def __init__(self):
+        super().__init__()
+        self.battery_pub_ = rospy.Publisher('/qcar/battery_state', BatteryState, queue_size=10)
+        self.carvel_pub_ = rospy.Publisher('/qcar/velocity', Vector3Stamped, queue_size=10)
+        self.imu_pub = rospy.Publisher("qcar_imu/raw", Imu, queue_size=10)
+        self.myCar = QCar()
+        self.sample_time = 0.001
+        self.command = np.array([0, 0])
+        self.cmd_sub_ = rospy.Subscriber('/qcar/user_command', Vector3Stamped, self.process_cmd, queue_size=100)
 #-------------------------------------------------------------------------------------------------
-	def looping(self):
+    def looping(self):
 
-		while not rospy.is_shutdown():
-			# Generate Commands
-			LEDs = np.array([0, 0, 0, 0, 0, 0, 0, 0])
-			# print(self.command)
-			# talk to QCar
-			current, batteryVoltage, encoderCounts = self.myCar.read_write_std(self.command, LEDs)
-
-
-			battery_state = BatteryState()
-			battery_state.header.stamp = rospy.Time.now()
-			battery_state.header.frame_id = 'battery_voltage'
-			battery_state.voltage = batteryVoltage
-			self.battery_pub_.publish(battery_state)
-
-			longitudinal_car_speed = basic_speed_estimation(encoderCounts)
-			velocity_state = Vector3Stamped()
-			velocity_state.header.stamp = rospy.Time.now()
-			velocity_state.header.frame_id = 'car_velocity'
-			velocity_state.vector.x = float(np.cos(self.command[1]) * longitudinal_car_speed)
-			velocity_state.vector.y = float(np.sin(self.command[1]) * longitudinal_car_speed)
-			self.carvel_pub_.publish(velocity_state)
-
-			time.sleep(self.sample_time)
+        while not rospy.is_shutdown():
+            # Generate Commands
+            LEDs = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+            # print(self.command)
+            # talk to QCar
+            current, batteryVoltage, encoderCounts = self.myCar.read_write_std(self.command, LEDs)
 
 
-		self.myCar.terminate()
+            battery_state = BatteryState()
+            battery_state.header.stamp = rospy.Time.now()
+            battery_state.header.frame_id = 'battery_voltage'
+            battery_state.voltage = batteryVoltage
+            self.battery_pub_.publish(battery_state)
+
+            longitudinal_car_speed = basic_speed_estimation(encoderCounts)
+            velocity_state = Vector3Stamped()
+            velocity_state.header.stamp = rospy.Time.now()
+            velocity_state.header.frame_id = 'car_velocity'
+            velocity_state.vector.x = float(np.cos(self.command[1]) * longitudinal_car_speed)
+            velocity_state.vector.y = float(np.sin(self.command[1]) * longitudinal_car_speed)
+            self.carvel_pub_.publish(velocity_state)
+
+            # publish imu to ros
+            self.my_car.read_IMU()
+            imu_msg = Imu()
+            imu_msg.header.stamp = rospy.Time.now()
+            imu_msg.linear_acceleration.x = self.my_car.read_other_buffer_IMU[3]
+            imu_msg.linear_acceleration.y = self.my_car.read_other_buffer_IMU[4]
+            imu_msg.linear_acceleration.z = self.my_car.read_other_buffer_IMU[5]
+            imu_msg.angular_velocity.x = self.my_car.read_other_buffer_IMU[0]
+            imu_msg.angular_velocity.y = self.my_car.read_other_buffer_IMU[1]
+            imu_msg.angular_velocity.z = self.my_car.read_other_buffer_IMU[2]
+            imu_msg.header.frame_id = "qcar_body"
+            imu_msg.orientation_covariance[0] = -1 # set to -1 to indicate that orientation is not available
+            imu_pub.publish(imu_msg)
+            time.sleep(self.sample_time)
 
 
-	def process_cmd(self, sub_cmd):
-		vel_cmd = sub_cmd.vector.x
-		str_cmd = sub_cmd.vector.y - 0.01
-		self.command = np.array([vel_cmd, str_cmd])
+        self.myCar.terminate()
+
+
+    def process_cmd(self, sub_cmd):
+        vel_cmd = sub_cmd.vector.x
+        str_cmd = sub_cmd.vector.y - 0.01
+        self.command = np.array([vel_cmd, str_cmd])
 
 
 
 if __name__ == '__main__':
-	rospy.init_node('qcar_node')
-	r = QcarNode()
-	r.looping()
-	rospy.spin()
+    rospy.init_node('qcar_node')
+    r = QcarNode()
+    r.looping()
+    rospy.spin()

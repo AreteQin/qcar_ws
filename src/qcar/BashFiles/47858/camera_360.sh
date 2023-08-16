7fe1b18696c0adeab5a17564ea1d24efb796d99d
Qin <you@example.com>
2023-08-16 10:20:35 -0400

    new ros files from quanser

--- src/qcar/BashFiles/47858/camera_360.sh
+++ src/qcar/BashFiles/47858/camera_360.sh
@@ -4,7 +4,7 @@ ls -l /dev |grep ttyTHS2
 sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
 source ~/qcar_ws/devel/setup.bash
-export $PYTHONPATH=$PYTHONPATH:/home/nvidia/qcar_ws/python
+export PYTHONPATH=$PYTHONPATH:/home/nvidia/qcar_ws/python
 export ROS_IP=192.168.1.95
 export ROS_MASTER_URI=http://192.168.1.12:11311
 sudo chmod +x /home/nvidia/qcar_ws/src/qcar/src/csinode.py

97eda3e0569e00be267095de3ddbf5d7257a48bd
Qin <you@example.com>
2023-08-18 20:48:23 -0400

    new ros files from quanser

---
+++ src/qcar/BashFiles/47858/d435_and_imu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ls -l /dev |grep ttyTHS2
+sudo chmod 666 /dev/ttyTHS2
+source /opt/ros/melodic/setup.bash
+source ~/qcar_ws/devel/setup.bash
+export PYTHONPATH=$PYTHONPATH:/home/nvidia/qcar_ws/python
+export ROS_IP=192.168.1.95
+export ROS_MASTER_URI=http://192.168.1.12:11311
+roslaunch qcar d435_and_imu.launch

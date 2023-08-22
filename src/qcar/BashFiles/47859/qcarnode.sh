9568c25f5e041e0e49312cc3ccd56e39a78903d5
Qin <you@example.com>
2023-08-22 11:12:37 -0400

    new ros files from quanser

--- src/qcar/BashFiles/47859/qcarnode.sh
+++ src/qcar/BashFiles/47859/qcarnode.sh
@@ -4,6 +4,7 @@ ls -l /dev |grep ttyTHS2
 sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
 source ~/qcar_ws/devel/setup.bash
+export PYTHONPATH=$PYTHONPATH:/home/nvidia/qcar_ws/python
 export ROS_IP=192.168.1.74
 export ROS_MASTER_URI=http://192.168.1.12:11311
 rosrun qcar qcarnode.py

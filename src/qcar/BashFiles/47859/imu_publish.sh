af825146250b4f3e64c7b4b2bb2bde6e3be27ff5
Qin <you@example.com>
2023-08-30 17:06:40 -0400

    imu test

--- src/qcar/BashFiles/47859/imu_publish.sh
+++ src/qcar/BashFiles/47859/imu_publish.sh
@@ -6,4 +6,4 @@ source /opt/ros/melodic/setup.bash
 source ~/qcar_ws/devel/setup.bash
 export ROS_IP=192.168.1.74
 export ROS_MASTER_URI=http://192.168.1.12:11311
-rosrun qcar imu_publisher.py
+rosrun qcar qcar_and_imu_node.py

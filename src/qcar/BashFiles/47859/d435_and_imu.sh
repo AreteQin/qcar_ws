affb9d465c4a12ddc72f607a80b5a118eec1b3f9
Qiaomeng Qin <you@example.com>
2023-12-19 12:35:32 -0500

    ip address updated

--- src/qcar/BashFiles/47859/d435_and_imu.sh
+++ src/qcar/BashFiles/47859/d435_and_imu.sh
@@ -5,6 +5,6 @@ sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
 source /media/nvidia/Sony32/qcar_ws/devel/setup.bash
 export PYTHONPATH=$PYTHONPATH:/home/nvidia/qcar_ws/python
-export ROS_IP=192.168.1.74
+export ROS_IP=192.168.1.75
 export ROS_MASTER_URI=http://192.168.1.12:11311
 roslaunch qcar d435_and_imu.launch
--- src/qcar/BashFiles/47859/imu_publish.sh
+++ src/qcar/BashFiles/47859/imu_publish.sh
@@ -4,6 +4,6 @@ ls -l /dev |grep ttyTHS2
 sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
 source /media/nvidia/Sony32/qcar_ws/devel/setup.bash
-export ROS_IP=192.168.1.74
+export ROS_IP=192.168.1.75
 export ROS_MASTER_URI=http://192.168.1.12:11311
 rosrun qcar qcar_and_imu_node.py

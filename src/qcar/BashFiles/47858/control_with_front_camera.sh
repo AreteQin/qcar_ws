a8bd5be5c20abfc2c3a07cf9e72e2f3c3048067f
Qiaomeng Qin <you@example.com>
2023-12-19 11:33:43 -0500

    file location updated

--- src/qcar/BashFiles/47858/control_with_front_camera.sh
+++ src/qcar/BashFiles/47858/control_with_front_camera.sh
@@ -5,6 +5,6 @@ sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
 source /media/nvidia/Sony32/qcar_ws/devel/setup.bash
 export PYTHONPATH=$PYTHONPATH:/media/nvidia/Sony32/qcar_ws/python
-export ROS_IP=192.168.1.74
+export ROS_IP=192.168.1.95
 export ROS_MASTER_URI=http://192.168.1.12:11311
 roslaunch qcar control_with_front_camera.launch

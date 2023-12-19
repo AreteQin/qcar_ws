8b4f175514febe788608a26d005f91d5e6ac8f33
Qiaomeng Qin <you@example.com>
2023-12-18 21:27:24 -0500

    file location updated

--- src/qcar/BashFiles/47859/d435_and_imu.sh
+++ src/qcar/BashFiles/47859/d435_and_imu.sh
@@ -3,7 +3,7 @@
 ls -l /dev |grep ttyTHS2
 sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
-source ~/qcar_ws/devel/setup.bash
+source /media/nvidia/Sony32/qcar_ws/devel/setup.bash
 export PYTHONPATH=$PYTHONPATH:/home/nvidia/qcar_ws/python
 export ROS_IP=192.168.1.74
 export ROS_MASTER_URI=http://192.168.1.12:11311

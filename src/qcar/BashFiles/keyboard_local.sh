acc89048bd454287ac7448208196f2684206e543
qin <qinqiaomeng@outlook.com>
2024-10-16 16:53:46 -0400

    keyboard local

--- src/qcar/BashFiles/keyboard_local.sh
+++ src/qcar/BashFiles/keyboard_local.sh
@@ -5,5 +5,4 @@ sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
 source /media/nvidia/Sony32/qcar_ws/devel/setup.bash
 export PYTHONPATH=$PYTHONPATH:/media/nvidia/Sony32/qcar_ws/python
-export PYTHONPATH=$PYTHONPATH:/home/nvidia/Documents/Quanser/libraries/python
 roslaunch qcar controller.launch

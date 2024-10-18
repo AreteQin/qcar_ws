4558981147b8f8548d9f9984a64bc0948bad872a
qin <qinqiaomeng@outlook.com>
2024-10-18 13:09:52 -0400

    workspace moved

--- src/qcar/BashFiles/keyboard_local.sh
+++ src/qcar/BashFiles/keyboard_local.sh
@@ -5,4 +5,5 @@ sudo chmod 666 /dev/ttyTHS2
 source /opt/ros/melodic/setup.bash
 source /home/nvidia/qcar_ws/devel/setup.bash
 export PYTHONPATH=$PYTHONPATH:/home/nvidia/qcar_ws/python
+chmod +x /home/nvidia/qcar_ws/src/qcar/src/qcarnode.py
 roslaunch qcar controller.launch
